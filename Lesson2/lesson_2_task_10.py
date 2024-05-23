50c261e3a891005714ec6f923d31445b4e9329c7
Anton Rudanov <tosha.rudanov@yandex.ru>
2024-05-23 12:16:36 +0300

    2 домашка

---
+++ Lesson2/lesson_2_task_10.py
@@ -0,0 +1,8 @@
+def bank (x,y):
+    for i in range (1, y+1):
+        count = x + (x/10)
+        x = count
+        print(round(count))
+x = int(input("Введите сумму вклада: "))
+y = int(input("Введите срок вклада: "))
+bank(x,y)          
---
+++ Lesson2/lesson_2_task_5.py
@@ -0,0 +1,13 @@
+def month_to_season(month):
+    if month in [12,1,2]:
+     return "Зима"
+    elif month in [3,4,5]:
+     return ("Весна")
+    if month in [6,7,8]:
+     return "Лето"
+    elif month in [9,10,11]:
+     return ("Осень")
+    else: 
+      return "Неверный номер месяца"
+month = int(input("Введите номер месяца: "))
+print("Название сезона: ", month_to_season(month))
